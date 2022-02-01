d7ac7918eda3b878fe01856e54f1d98699cc28d9
Furkan Cengiz <76488131+furkancengiz6@users.noreply.github.com>
2022-02-01 11:57:00 +0300

    Add files via upload

---
+++ DictionaryIntro/MyDictionary.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryIntro
+{
+    class MyDictionary<TKey, TValue>
+    {
+        TKey[] keys;
+        TValue[] values;
+
+        public MyDictionary()
+        {
+            keys = new TKey[0];
+            values = new TValue[0];  
+        }
+        public void Add(TKey key , TValue value)
+        {
+            TKey[] tempKey = keys;
+            TValue[] tempValue = values;
+            keys = new TKey[keys.Length+1];
+            values = new TValue[keys.Length+1];
+            for (int i = 0; i < tempKey.Length; i++)
+            {
+                keys[i] = tempKey[i];
+               
+            }
+            for (int j = 0; j < tempValue.Length; j++)
+            {
+                values[j] = tempValue[j];
+            }
+            keys[keys.Length-1] = key;
+            values[values.Length-1] =value;
+            
+        }
+       public int Length
+        { get { return keys.Length; } }
+        public void Print() {
+            for (int i = 0; i < keys.Length; i++)
+            {
+Console.WriteLine($"Key : {keys[i]} | Value :{values[i]}");
+            }
+            
+        }        
+
+    }
+}
