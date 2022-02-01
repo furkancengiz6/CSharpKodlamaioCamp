8ac33a7ea95871285ebdf5bb99a49eb3a932b641
Furkan Cengiz <76488131+furkancengiz6@users.noreply.github.com>
2022-02-01 11:59:08 +0300

    Add files via upload

---
+++ ClassMetotDemo/CustomerManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    internal class CustomerManager
+    {
+        public void Add(Customer customer)
+        {
+            Console.WriteLine("Musteri ismi :" + customer.Name + "Musteri Soyismi" + customer.Surname + "TC No:" + customer.TcNo+"/n"+"Müşteri Eklendi");
+        }
+        public void Delete(Customer customer)
+        {
+            Console.WriteLine("Musteri ismi :" + customer.Name + "Musteri Soyismi" + customer.Surname + "TC No:" + customer.TcNo + "/n" + "Müşteri Silindi");
+        }
+        public void CustomersList(Customer[] customers)
+        {
+            foreach (Customer customer in customers)
+            {
+                Console.WriteLine("Musteri Adı:" + customer.Name);
+            }
+        }
+
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,28 @@
+﻿using ClassMetotDemo;
+using System;
+
+namespace MyNamespace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer = new Customer();
+            customer.Name = "Furkan";
+            customer.Surname = "Cengiz";
+            Customer customer2 = new Customer();
+            customer2.Name = "Ali";
+            customer2.Surname = "Veli";
+            Customer customer3 = new Customer();
+            customer3.Name = "Mehmet";
+            customer.Surname = "asdsa";
+
+
+            CustomerManager manager = new CustomerManager();
+            manager.Add(customer);
+            Customer[] customers = new Customer[] { customer, customer2 };
+            manager.CustomersList(customers);
+
+        }
+    }
+}
